b3247f9b714e3f4f52c5a69483cb0791174346b4
Max Zhao <max.zhao@arsbrevis.de>
2023-05-26 10:57:25 +0200

    Adding correct script to generate echtvar gnomad annotations

--- generate_gnomad.sh
+++ generate_gnomad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echtvar encode gnomad_v202_exomes.zip conf_gnomad_exomes.json "/fast/projects/cubit/current/static_data/db/gnomAD/release2.0.2/GRCh37/gnomad.exomes.r2.0.2.sites.vcf.gz"
+HUMREF=/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa
 
-echtvar encode gnomad_v202_genomes.zip conf_gnomad_genomes.json "/fast/projects/cubit/current/static_data/db/gnomAD/release2.0.2/GRCh37/gnomad.genomes.r2.0.2.sites.vcf.gz"
+bcftools norm -m - -w 10000 -f $HUMREF -O b "/fast/projects/cubit/current/static_data/db/gnomAD/release2.0.2/GRCh37/gnomad.exomes.r2.0.2.sites.vcf.gz" | echtvar encode ../gnomad_v202_exomes.zip conf_gnomad_exomes.json -
+
+bcftools norm -m - -w 10000 -f $HUMREF -O b "/fast/projects/cubit/current/static_data/db/gnomAD/release2.0.2/GRCh37/gnomad.genomes.r2.0.2.sites.vcf.gz" | echtvar encode ../gnomad_v202_genomes.zip conf_gnomad_genomes.json -
